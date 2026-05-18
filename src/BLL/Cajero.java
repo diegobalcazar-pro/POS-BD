0d3752b418ed64d94e56e53235f307021e82c747
Balcaz <232366252+diegobalcazar-pro@users.noreply.github.com>
2026-05-18 12:24:20 -0300

    feat: Menu de Cajero

--- src/BLL/Cajero.java
+++ src/BLL/Cajero.java
@@ -18,23 +18,159 @@ public class Cajero extends Usuario {
 	public String toString() {
 		return "Cajero [toString()=" + super.toString() + "]";
 	}
+	
 	@Override
+	//------------------------------------------------------------------ MENU DE CAJERO ----------------------------------------------------------------------------------------------------------------
 	public void Menu() {
 
-		String[] opciones = { "Ver usuarios", "Salir" };
+		String[] opciones = { "Realizar Venta", "Ver Caja", "Ver Stock", "Ver Ventas", "Cerra Caja", "Cerrar Sesion" };
+		
 		int opcion;
 		do {
 
 			opcion = JOptionPane.showOptionDialog(null, "Seleccione una opción", "", 0, 0, null, opciones, opciones);
 			switch (opcion) {
 			case 0:
-				JOptionPane.showMessageDialog(null, this.getController().mostrarUsuarios());
+				//REALIZAR VENTA
+				String[] opciones_gestion_usuario = { "Cliente", "Agregar Producto", "Borrar Producto", "Agregar Descuento", "Procesar Cobro", "← Salir" };
+				int opcion_gestionar_usuario;
+				do {
+					opcion_gestionar_usuario = JOptionPane.showOptionDialog(null, "Seleccione una opción", "", 0, 0, null, opciones_gestion_usuario, opciones_gestion_usuario);
+					switch (opcion_gestionar_usuario) {
+					case 0:
+						//CLIENTE
+						
+						
+						break;
+					case 1:
+						//AGREGAR PRODUCTOS
+						
+						
+						break;
+					case 2:
+						//BORRAR PRODUCTOS
+						
+						
+						break;
+					case 3:
+						//AGREGAR DESCUENTO
+						
+						
+						break;
+					case 4:
+						//PROCESAR COBRO
+						
+						
+						break;
+					default:
+						break;
+					}
+					
+				} while (opcion_gestionar_usuario != 5); //SALE DE REALIZAR VENTA
+				
+				
+				break;
+			case 1:
+				//VER CAJA
+				String[] opciones_gestion_productos = { "Imprimir Dia", "Añadir Gasto", "← Salir" };
+				int opcion_gestionar_productos;
+				do {
+					opcion_gestionar_productos = JOptionPane.showOptionDialog(null, "Seleccione una opción", "", 0, 0, null, opciones_gestion_productos, opciones_gestion_productos);
+					switch (opcion_gestionar_productos) {
+					case 0:
+						//IMPRIMIR DIA
+						
+						break;
+					case 1:
+						//AÑADIR GASTO
+						
+						break;
+
+					default:
+						break;
+					}
+					
+				} while (opcion_gestionar_productos != 2); //SALE DE VER CAJA
+				
+				break;
+			case 2:
+				//VER STOCK
+				String[] opciones_info_ventas = { "Ver Todo", "Buscar Producto", "← Salir" };
+				int opcion_info_ventas;
+				do {
+					opcion_info_ventas = JOptionPane.showOptionDialog(null, "Seleccione una opción", "", 0, 0, null, opciones_info_ventas, opciones_info_ventas);
+					switch (opcion_info_ventas) {
+					case 0:
+						//VER TODO
+						
+						break;
+					case 1:
+						//BUSCAR PRODUCTO
+						
+						break;
+
+					default:
+						break;
+					}
+					
+				} while (opcion_info_ventas != 2); //SALE DE VER STOCK
+				
+				break;
+			case 3:
+				//VER VENTAS
+				String[] opciones_config = { "Por Fecha", "Por Cliente", "Por N° Venta", "← Salir" };
+				int opcion_config;
+				do {
+					opcion_config = JOptionPane.showOptionDialog(null, "Seleccione una opción", "", 0, 0, null, opciones_config, opciones_config);
+					switch (opcion_config) {
+					case 0:
+						//POR FECHA
+						
+						break;
+					case 1:
+						//POR CLIENTE
+						
+						break;
+					case 2:
+						//POR N°VENTA
+						
+						break;
+
+					default:
+						break;
+					}
+					
+				} while (opcion_config != 3); //SALE DE VER VENTAS
+				
+				break;	
+			case 4:
+				//CERRAR CAJA
+				String[] opciones_cerrar_caja = { "Si", "← No, Salir" };
+				int opcion_cerrar_caja;
+				do {
+					opcion_cerrar_caja = JOptionPane.showOptionDialog(null, "¿Esta Seguro?", "Cerrar Caja", 0, 0, null, opciones_cerrar_caja, opciones_cerrar_caja);
+					switch (opcion_cerrar_caja) {
+					case 0:
+						//SI, CERRAR CAJA
+						
+						opcion_cerrar_caja = 1;
+						
+						break;
+
+					default:
+						break;
+					}
+					
+				} while (opcion_cerrar_caja != 1); //SALE DE CERRAR CAJA
+				
+				break;
 
 			default:
 				break;
 			}
-		} while (opcion != 2);
-		
+			
+		} while (opcion != 5); //CIERRA SESION DE CAJERO
+
 	}
     
     
