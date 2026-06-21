d684c4518a9885d536bb384573de6b2204de9224
ianjapan-cell <ian.japan@davinci.edu.ar>
2026-06-21 20:15:00 -0300

    feat(MenuGestionProveedores): GUI maquetado

---
+++ src/GUI/MenuGestionProveedores.java
@@ -0,0 +1,73 @@
+package GUI;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JSeparator;
+import java.awt.Color;
+import javax.swing.JTable;
+import javax.swing.JButton;
+
+public class MenuGestionProveedores extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MenuGestionProveedores frame = new MenuGestionProveedores();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MenuGestionProveedores() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 572, 410);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		table = new JTable();
+		table.setBounds(10, 11, 538, 216);
+		contentPane.add(table);
+		
+		JButton btnCrearProv = new JButton("Crear");
+		btnCrearProv.setBounds(169, 296, 88, 22);
+		contentPane.add(btnCrearProv);
+		
+		JButton btnElimProv = new JButton("Eliminar");
+		btnElimProv.setBounds(40, 296, 88, 22);
+		contentPane.add(btnElimProv);
+		
+		JButton btnModiProv = new JButton("Modificar");
+		btnModiProv.setBounds(298, 296, 88, 22);
+		contentPane.add(btnModiProv);
+		
+		JButton btnAtras = new JButton("Atrás");
+		btnAtras.setBounds(427, 296, 88, 22);
+		contentPane.add(btnAtras);
+		
+		JSeparator separator = new JSeparator();
+		separator.setForeground(Color.BLACK);
+		separator.setBounds(10, 238, 538, 124);
+		contentPane.add(separator);
+
+	}
+
+}
