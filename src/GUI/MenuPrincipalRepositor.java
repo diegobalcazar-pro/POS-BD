8f6531abe3fdc36e06e9b4f1c01abace32d1229b
ianjapan-cell <ian.japan@davinci.edu.ar>
2026-06-21 23:23:17 -0300

    feat(MenuPrincipalRepositor): botones funcionales

--- src/GUI/MenuPrincipalRepositor.java
+++ src/GUI/MenuPrincipalRepositor.java
@@ -45,37 +45,64 @@ public class MenuPrincipalRepositor extends JFrame {
 		contentPane.setLayout(null);
 
 		JButton botonGstProd = new JButton("Gestión Productos");
-		botonGstProd.setBounds(150, 183, 133, 23);
+		botonGstProd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MenuGestionProductos ventanaProductos = new MenuGestionProductos();
+		        ventanaProductos.setVisible(true);
+		        dispose();
+			}
+		});
+		botonGstProd.setBounds(112, 183, 162, 23);
 		contentPane.add(botonGstProd);
 
 		JButton botonGstPed = new JButton("Gestión Pedidos");
-		botonGstPed.setBounds(293, 183, 133, 23);
+		botonGstPed.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MenuGestionPedidos ventanaProductos = new MenuGestionPedidos();
+		        ventanaProductos.setVisible(true);
+		        dispose();
+			}
+		});
+		botonGstPed.setBounds(282, 183, 162, 23);
 		contentPane.add(botonGstPed);
 
 		JButton botonGstStock = new JButton("Gestión Stock");
 		botonGstStock.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				MenuGestionStock ventanaProductos = new MenuGestionStock();
+		        ventanaProductos.setVisible(true);
+		        dispose();
 			}
 		});
-		botonGstStock.setBounds(150, 217, 133, 23);
+		botonGstStock.setBounds(112, 217, 162, 23);
 		contentPane.add(botonGstStock);
 
 		JButton botonCerrarSes = new JButton("Cerrar sesión");
 		botonCerrarSes.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				Login ventanaProductos = new Login();
+		        ventanaProductos.setVisible(true);
+		        dispose();
 			}
 		});
-		botonCerrarSes.setBounds(221, 251, 133, 22);
+		botonCerrarSes.setBounds(205, 251, 147, 22);
 		contentPane.add(botonCerrarSes);
-
-		JButton botonGstProv = new JButton("Gestión Proveedores");
-		botonGstProv.setBounds(293, 216, 133, 23);
-		contentPane.add(botonGstProv);
 		
 		JLabel lblNewJgoodiesLabel = DefaultComponentFactory.getInstance().createLabel("¡Bienvenido, Repositor [nombre]!");
 		lblNewJgoodiesLabel.setFont(new Font("Tahoma", Font.PLAIN, 24));
 		lblNewJgoodiesLabel.setBounds(99, 104, 360, 52);
 		contentPane.add(lblNewJgoodiesLabel);
+		
+		JButton botonGstProv = new JButton("Gestión Proveedores");
+		botonGstProv.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MenuGestionProveedores ventanaProductos = new MenuGestionProveedores();
+		        ventanaProductos.setVisible(true);
+		        dispose();
+			}
+		});
+		botonGstProv.setBounds(282, 217, 162, 23);
+		contentPane.add(botonGstProv);
 
 	}
 }
