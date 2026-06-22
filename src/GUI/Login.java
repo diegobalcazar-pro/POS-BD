bf49f4fd08573fc67248886e840b9a40ba5795d8
Balcaz <232366252+diegobalcazar-pro@users.noreply.github.com>
2026-06-22 10:11:52 -0300

    fix:(Login)

--- src/GUI/Login.java
+++ src/GUI/Login.java
@@ -5,24 +5,34 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JLayeredPane;
-import javax.swing.JTextField;
+
+import BLL.Admin;
+import BLL.Cajero;
+import BLL.Repositor;
+import BLL.Usuario;
+import DLL.ControllerUsuario;
+
 import javax.swing.JLabel;
-import javax.swing.SwingConstants;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
-import javax.swing.ImageIcon;
-import java.awt.Color;
-import javax.swing.JSeparator;
-import javax.swing.JTabbedPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
+import java.awt.Toolkit;
+import java.awt.Button;
 
 public class Login extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField;
-	private JPasswordField passwordField;
+	private JTextField inpEmail;
+	private JPasswordField inpContrasenia;
 
 	/**
 	 * Launch the application.
@@ -44,71 +54,116 @@ public class Login extends JFrame {
 	 * Create the frame.
 	 */
 	public Login() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\Intel I5\\Downloads\\92b80f55c7e3a2476cc2ff9481e357c3.jpg"));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 572, 410);
+		setBounds(100, 100, 742, 551);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(10, 21, 536, 339);
-		contentPane.add(tabbedPane);
-		
-		JPanel panel_1 = new JPanel();
-		tabbedPane.addTab("Login", null, panel_1, null);
-		panel_1.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("LOGIN | POS");
-		lblNewLabel.setBounds(10, 0, 186, 42);
-		panel_1.add(lblNewLabel);
-		lblNewLabel.setHorizontalAlignment(SwingConstants.LEFT);
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		
-		textField = new JTextField();
-		textField.setColumns(10);
-		textField.setBounds(10, 101, 166, 27);
-		panel_1.add(textField);
-		
-		JLabel lblNewLabel_1 = new JLabel("Correo:");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblNewLabel_1.setBounds(10, 65, 77, 27);
-		panel_1.add(lblNewLabel_1);
-		
-		JLabel lblNewLabel_1_1 = new JLabel("Contraseña:");
-		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblNewLabel_1_1.setBounds(10, 137, 121, 27);
-		panel_1.add(lblNewLabel_1_1);
-		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(10, 177, 166, 27);
-		panel_1.add(passwordField);
-		
-		JLabel lblNewLabel_2 = new JLabel("");
-		lblNewLabel_2.setIcon(new ImageIcon("C:\\Users\\Intel I5\\Downloads\\e2b0d189-dd63-4116-930e-50b2dc34f23a.jpg"));
-		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2.setBackground(Color.WHITE);
-		lblNewLabel_2.setBounds(263, -12, 268, 334);
-		panel_1.add(lblNewLabel_2);
-		
-		JButton btnNewButton = new JButton("Iniciar Sesion");
-		btnNewButton.setBounds(10, 230, 121, 27);
-		panel_1.add(btnNewButton);
-		
-		JButton btnSalir = new JButton("Salir");
-		btnSalir.setBounds(10, 261, 121, 27);
-		panel_1.add(btnSalir);
-		
-		JSeparator separator = new JSeparator();
-		separator.setToolTipText("hola");
-		separator.setOrientation(SwingConstants.VERTICAL);
-		separator.setForeground(Color.BLACK);
-		separator.setBackground(Color.BLACK);
-		separator.setBounds(258, 64, 29, 190);
-		panel_1.add(separator);
-		
-		JPanel panel = new JPanel();
-		tabbedPane.addTab("Registrarse", null, panel, null);
 
+		JLabel lblNewLabel_1 = new JLabel("Correo:");
+		lblNewLabel_1.setForeground(new Color(0, 0, 0));
+		lblNewLabel_1.setFont(new Font("Verdana", Font.BOLD, 15));
+		lblNewLabel_1.setBounds(445, 179, 220, 14);
+		contentPane.add(lblNewLabel_1);
+		
+				inpEmail = new JTextField();
+				inpEmail.setToolTipText("correo");
+				inpEmail.setBounds(445, 205, 220, 30);
+				contentPane.add(inpEmail);
+				inpEmail.setColumns(10);
+
+		JLabel lblNewLabel_1_1 = new JLabel("Contraseña:");
+		lblNewLabel_1_1.setFont(new Font("Verdana", Font.BOLD, 15));
+		lblNewLabel_1_1.setBounds(445, 261, 220, 14);
+		contentPane.add(lblNewLabel_1_1);
+
+		inpContrasenia = new JPasswordField();
+		inpContrasenia.setBounds(445, 286, 220, 30);
+		contentPane.add(inpContrasenia);
+
+		JLabel lblError = new JLabel("");
+		lblError.setForeground(Color.RED);
+		lblError.setBounds(40, 355, 265, 32);
+		contentPane.add(lblError);
+		JButton btnLogin = new JButton("Ingresar");
+		btnLogin.setBackground(new Color(0, 64, 128));//fondo del boton
+		btnLogin.setForeground(new Color(0, 0, 0));
+		btnLogin.setFont(new Font("Segoe UI", Font.BOLD, 11));
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				Usuario logueado = Usuario.Login(inpEmail.getText(), inpContrasenia.getText());
+				if (logueado == null) {
+					lblError.setText("No se encontró");
+				} else {
+					
+			
+					Login frame = new Login();
+					frame.setVisible(false);
+					dispose();
+						
+					System.out.println("Bienvenido " + logueado.getNombre_usuario());
+					
+				
+                    if (logueado != null) {
+                        if (logueado instanceof Admin) {
+                            JOptionPane.showMessageDialog(null, "Bienvenido Admin " + logueado.getNombre_usuario());
+                            // Ir a menu de admin
+                            logueado.Menu();
+                        } else if (logueado instanceof Cajero) {
+                            JOptionPane.showMessageDialog(null, "Bienvenido Cajero " + logueado.getNombre_usuario());
+                            // Ir a menu de cajero
+                            logueado.Menu();
+                        } else if (logueado instanceof Repositor) {
+                        	JOptionPane.showMessageDialog(null, "Bienvenido Repositor " + logueado.getNombre_usuario());
+                            // Ir a menu de repositor
+                        	logueado.Menu();
+                        }
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Usuario o contraseña incorrectos");
+                    }
+					
+					
+				}
+			}
+		});
+		btnLogin.setBounds(445, 349, 220, 23);
+		contentPane.add(btnLogin);
+		
+		JLabel lblNewLabel_1_2 = new JLabel("LOGIN | POS");
+		lblNewLabel_1_2.setFont(new Font("Arial Black", Font.PLAIN, 25));
+		lblNewLabel_1_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_2.setBounds(434, 93, 237, 32);
+		contentPane.add(lblNewLabel_1_2);
+		
+				JLabel lblNewLabel = new JLabel("");
+				lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+				lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Intel I5\\Downloads\\fondologin.jpg"));
+				lblNewLabel.setBounds(0, 0, 385, 512);
+				contentPane.add(lblNewLabel);
+				
+				JLabel lblNewLabel_1_2_1 = new JLabel("Bienvenido");
+				lblNewLabel_1_2_1.setForeground(new Color(128, 128, 128));
+				lblNewLabel_1_2_1.setHorizontalAlignment(SwingConstants.CENTER);
+				lblNewLabel_1_2_1.setFont(new Font("Arial Black", Font.PLAIN, 13));
+				lblNewLabel_1_2_1.setBounds(442, 121, 237, 32);
+				contentPane.add(lblNewLabel_1_2_1);
+		
+		/*
+		JButton btnRegistrar = new JButton("registrar");
+		btnRegistrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				DLLUsuario controller = new DLLUsuario();
+				controller.Registrarse(new Usuario("ghami","ghami@gmail.com","Profesor","1234"));
+			}
+		});
+		btnRegistrar.setBounds(184, 452, 121, 23);
+		contentPane.add(btnRegistrar);
+*/
+		
 	}
 }
